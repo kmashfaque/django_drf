a0ecf84fea765e6d5e50cbfca13802af13a63baf
ashfaque <rahamankmashfaque@gmail.com>
2025-12-10 02:18:49 +0600

    get details by pk

--- api/views.py
+++ api/views.py
@@ -19,10 +19,29 @@ from rest_framework.decorators import api_view
 
 # automatic serialization from drf
 
-@api_view(['GET'])
+@api_view(['GET', 'POST'])
 def studentsView(request):
     if request.method == 'GET':
-        #lget all the data from the students table
+        #get all the data from the students table
         student = Student.objects.all()
         serializer = StudentSerializer(student, many = True)
-        return Response(serializer.data, status = status.HTTP_200_OK)
+        return Response(serializer.data, status = status.HTTP_200_OK)   
+    
+    elif request.method == 'POST':
+        serializer = StudentSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        print(serializer.errors)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+@api_view(['GET'])
+def studentDetailView(resquest, pk):
+    try:
+        student = Student.objects.get(pk=pk)
+    except Student.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+    
+    if resquest.method == 'GET':
+        serializer = StudentSerializer(student)
+        return Response(serializer.data, status=status.HTTP_200_OK)
