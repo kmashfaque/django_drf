552f6211e5d2a27e94081244f7ee6ee2e6739864
ashfaque <rahamankmashfaque@gmail.com>
2026-01-31 21:54:03 +0600

    employee model updated

--- api/views.py
+++ api/views.py
@@ -66,5 +66,11 @@ class Employees(APIView):
         serializer = EmployeeSerializer(employees, many=True)
         return Response(serializer.data, status=status.HTTP_200_OK)
 
-
-
+    def post(self, request):
+       
+        serializer = EmployeeSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            print(request.data)
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
